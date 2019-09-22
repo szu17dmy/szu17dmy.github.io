1e7573ee32cce76ba41e104f2150bd5a38eacf8e
domain <szu.dmy@qq.com>
2019-09-22 19:36:23 +0800

    ADD szu_oj/ds/1024-19

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -8,3 +8,24 @@ window.addEventListener('load', function () {
         document.body.setAttribute('data-md-color-accent', mdColorAccent);
     }
 }, false);
+
+window.MathJax = {
+    tex2jax: {
+        inlineMath: [ ["\\(","\\)"] ],
+        displayMath: [ ["\\[","\\]"] ]
+    },
+    TeX: {
+        TagSide: "right",
+        TagIndent: ".8em",
+        MultLineWidth: "85%",
+        equationNumbers: {
+            autoNumber: "AMS",
+        },
+        unicode: {
+            fonts: "STIXGeneral,'Arial Unicode MS'"
+        }
+    },
+    displayAlign: "left",
+    showProcessingMessages: false,
+    messageStyle: "none"
+};
